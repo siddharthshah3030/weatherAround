159b24dff50dd7401e6ae75e7e1cc61a46ebd6cf
Your Name <siddharthshah3030@gmail.com>
2018-06-10 05:02:46 +0530

    weekdays working

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ app.get("/", function(req, res){
 app.get("/results", function(req, res){
     var query = req.query.city;
     var url = "http://api.openweathermap.org/data/2.5/weather?q=" + query+ "&appid=8f046bdc3838e6b489a20d5225eba357";
+    var furl = "http://api.openweathermap.org/data/2.5/forecast?q=" + query+ "&appid=8f046bdc3838e6b489a20d5225eba357";
+
+
 
   console.log(url);
   request(url, function(error, response, body){
@@ -28,18 +31,35 @@ app.get("/results", function(req, res){
             console.log(typeof(ob));
             var weather = JSON.stringify(ob);
             console.log(weather);
-            res.render("nr", {data: ob});
             console.log(ob.name);
+      request(furl, function(error, response, body){
+        var fob = JSON.parse(body);
+        console.log(typeof(ob));
+        var weather = JSON.stringify(fob);
+        res.render("nr", {data: ob, fdata: fob});
+        console.log(weather);
+      })
+
         }
     });
 });
 
 
-
-app.listen(8000, () => {
+// var http=require('http');
+//
+// var server=http.createServer(function(req,res){
+//     res.end('test');
+// });
+//
+// server.on('listening',function(){
+//     console.log('ok, server is running');
+// });
+//
+// app.listen(8000);
+app.listen(3000, () => {
   console.log("SiD server started");
 });
-//
+
 // app.listen(process.env.PORT, process.env.IP, function(){
 //     console.log("weather webapp has started!!!");
 // });
